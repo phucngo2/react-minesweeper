9e1cb5593df4ba8111749fb99c14e652a04c37e6
phucngo2 <phucnnh21@gmail.com>
2024-08-07 20:06:39 +0700

    Feature(24): Game controls

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,5 +4,14 @@ import { useAtomValue } from "jotai";
 export const Header = () => {
   const gameState = useAtomValue(gameStateAtom);
 
-  return <div className="font-semibold">{gameState}</div>;
+  return (
+    <div className="flex flex-row items-center justify-between gap-4 font-semibold">
+      <div className="flex flex-row items-center gap-2.5">
+        <button className="w-10 h-10 min-h-0 btn">⚙️</button>
+        <button className="w-10 h-10 min-h-0 btn">🙂</button>
+        <div>{gameState}</div>
+      </div>
+      <div>00:00</div>
+    </div>
+  );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,8 @@ interface Props {
 
 export const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="flex flex-col items-start justify-center gap-4 p-6 overflow-auto lg:items-center w-dvw h-dvh">
-      {children}
+    <div className="flex flex-col items-start justify-center p-6 overflow-auto lg:items-center w-dvw h-dvh">
+      <div className="flex flex-col gap-4">{children}</div>
     </div>
   );
 };
