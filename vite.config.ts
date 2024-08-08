e59d2556571cc46d54000ac4147f455af1718b6f
phucngo2 <phucnnh21@gmail.com>
2024-08-08 23:03:11 +0700

    Build(49): Deploy using gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tsconfigPaths()],
+  base: "",
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
