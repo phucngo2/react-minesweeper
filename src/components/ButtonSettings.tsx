79acc21fef0acc9936c0c108ec58c6fa26ed8236
phucngo2 <phucnnh21@gmail.com>
2024-08-08 20:43:29 +0700

    Feature(39): Settings modal

--- src/components/ButtonSettings.tsx
+++ src/components/ButtonSettings.tsx
@@ -1,7 +1,36 @@
+import { useRef } from "react";
+
 export const ButtonSettings = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const handleClick = () => {
+    dialogRef.current?.showModal();
+  };
+
   return (
-    <div className="tooltip tooltip-bottom" data-tip="Settings">
-      <button className="w-10 h-10 min-h-0 text-xl btn">⚙️</button>
-    </div>
+    <>
+      <div className="tooltip tooltip-bottom" data-tip="Settings">
+        <button onClick={handleClick} className="w-10 h-10 min-h-0 text-xl btn">
+          ⚙️
+        </button>
+      </div>
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+        {/* Modal backdrop to handle close when click outside */}
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 };
