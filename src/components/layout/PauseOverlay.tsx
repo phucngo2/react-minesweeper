588d285046cc11b9406a80429da023c99a2be3a6
phucngo2 <phucnnh21@gmail.com>
2024-08-09 17:19:01 +0700

    Feature(52): Hide the board when game pause

--- src/components/layout/PauseOverlay.tsx
+++ src/components/layout/PauseOverlay.tsx
@@ -1,11 +1,15 @@
 import { GameStates } from "@app/config";
 import { gameStateAtom } from "@app/stores";
 import { useAtomValue } from "jotai";
+import ImgNun from "@app/assets/nun.jpg";
 
 export const PauseOverlay = () => {
   const gameState = useAtomValue(gameStateAtom);
   if (gameState != GameStates.Paused) return <></>;
   return (
-    <div className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full text-6xl opacity-15 bg-neutral-content card"></div>
+    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full p-4 z-5 bg-neutral card">
+      <img src={ImgNun} className="rounded-lg w-full max-w-[240px]" />
+      <div className="mt-2 font-semibold">🦌 NUN!</div>
+    </div>
   );
 };
